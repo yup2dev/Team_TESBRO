363c8d24fbfd27a284bf326f8d24457fcaa69739
Jserin <jsr3643@naver.com>
2023-06-05 13:47:04 +0900

    academyStart1

--- src/main/java/com/team/tesbro/Academy/Academy.java
+++ src/main/java/com/team/tesbro/Academy/Academy.java
@@ -1,9 +1,6 @@
 package com.team.tesbro.Academy;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,4 +11,18 @@ public class Academy {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(unique = true)
+    private String academyName;
+
+    @Column(length = 100)
+    private String ceoName;
+
+    @Column
+    private String academyAddress;
+
+    @Column
+    private long academyNum;
+
+
 }
