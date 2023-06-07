59b21eb71da98c61d53f977f53a854f689e3cebd
Jserin <jsr3643@naver.com>
2023-06-07 23:45:14 +0900

    teacher-ing2

--- src/test/java/com/team/tesbro/TesbroApplicationTests.java
+++ src/test/java/com/team/tesbro/TesbroApplicationTests.java
@@ -30,12 +30,51 @@ class TesbroApplicationTests {
 		Academy a = oa.get();
 
 		Teacher t = new Teacher();
-		t.setTeacherName("이승열");
-		t.setQualifications("홈프로텍터 과정 수료");
-		t.setAwards("집에 빨리가기 대회 우승");
-		t.setIntroduction("안녕하세요. 왤캐 바쁘세요. 쉬엄쉬엄 일정 잘 마치시고 과제는 까먹지말고 하세요. 예외없음");
+		t.setTeacherName("전세린");
+		t.setQualifications("수상한 경력");
+		t.setAwards("의심스러운 수상내역");
+		t.setIntroduction("노는게 제일 좋아. 친구들 모여라. 언제나 즐거워 개구쟁이 뽀로로.");
 		t.setAcademy(a);
 		this.teacherRepository.save(t);
+
+		Teacher t2 = new Teacher();
+		t2.setTeacherName("김다은");
+		t2.setQualifications("카페 운영 56년차");
+		t2.setAwards("클라이언트블락 자격증 보유");
+		t2.setIntroduction("눈 덮인 숲속마을. 꼬마 펭귄 나가신다. 언제나 즐거워 오늘은 또 무슨 일이 생길까.");
+		t2.setAcademy(a);
+		this.teacherRepository.save(t2);
+
+		Teacher t3 = new Teacher();
+		t3.setTeacherName("김상엽");
+		t3.setQualifications("다리 분쇄 2주차");
+		t3.setAwards("한발 멀리뛰기 대회 금메달 수상");
+		t3.setIntroduction("뽀로로를 불러봐요. 뽀롱뽀롱 뽀로로. 뽀롱뽀롱 뽀로로. 뽀롱뽀롱뽀롱뽀롱뽀롱뽀롱뽀롱 뽀로로.");
+		t3.setAcademy(a);
+		this.teacherRepository.save(t3);
+
+		Teacher t4 = new Teacher();
+		t4.setTeacherName("이승열");
+		t4.setQualifications("홈프로텍터 과정 수료");
+		t4.setAwards("집에 빨리가기 대회 우승");
+		t4.setIntroduction("노는게 제일 좋아. 친구들 모여라. 언제나 즐거워. 뽀롱뽀롱뽀롱뽀롱 뽀 로 로!");
+		t4.setAcademy(a);
+		this.teacherRepository.save(t4);
 	}
 
+	@Test
+	void contextLoads2() {
+		Optional<Academy> oa = this.academyRepository.findById(2);
+		assertTrue(oa.isPresent());
+		Academy a = oa.get();
+
+		Teacher t = new Teacher();
+		t.setTeacherName("테스트");
+		t.setQualifications("테스트 경력");
+		t.setAwards("테스트 수상내역");
+		t.setIntroduction("테스트 자기소개 으아아아아아악");
+		t.setAcademy(a);
+		this.teacherRepository.save(t);
+
+	}
 }
