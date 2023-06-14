5443a76e14e49852f69fd3690b9053e519718559
êãsangyeopK <creativethony@naver.com>
2023-06-14 17:41:46 +0900

    미쳐

--- src/main/java/com/team/tesbro/Lesson/LessonController.java
+++ src/main/java/com/team/tesbro/Lesson/LessonController.java
@@ -6,16 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.*;
 
 @Controller
 @RequiredArgsConstructor
 public class LessonController {
     private final LessonService lessonService;
-
+    private List<LocalDate> uniqueDateList;
+    private List<Lesson> lessonTimeList;
+    private List<LocalTime> uniqueTimeList;
 
     @GetMapping("/academy/lesson")
     public String handleSelectedData(Model model) {
+
         List<Lesson> lessonList = lessonService.getList();
         List<LocalDate> dateList = new ArrayList<>();
         for (Lesson lesson : lessonList) {
@@ -23,15 +27,38 @@ public class LessonController {
             dateList.add(lessonDate);
         }
         Set<LocalDate> uniqueDates = new HashSet<>(dateList);
-        List<LocalDate> uniqueDateList = new ArrayList<>(uniqueDates);
+        uniqueDateList = new ArrayList<>(uniqueDates);
         Collections.sort(uniqueDateList);
         model.addAttribute("uniqueDateList", uniqueDateList);
+        // 데이터 날짜 정렬 및 모델에 출력
+
+        if (lessonTimeList == null || lessonTimeList.isEmpty()) {
+            // lessonTimeList가 null 또는 비어 있는 경우 처리
+            uniqueTimeList = new ArrayList<>();
+            uniqueTimeList.add(LocalTime.of(0, 0));
+            model.addAttribute("uniqueTimeList", uniqueTimeList);
+        } else {
+            // lessonTimeList에 값이 있는 경우 처리
+            List<LocalTime> timeList = new ArrayList<>();
+            for (Lesson lesson2 : lessonTimeList) {
+                LocalTime lessonTime = lesson2.getLessonTime();
+                timeList.add(lessonTime);
+            }
+
+            Set<LocalTime> uniqueTimes = new HashSet<>(timeList);
+            System.out.println(uniqueTimes);
+            uniqueTimeList = new ArrayList<>(uniqueTimes);
+            Collections.sort(uniqueTimeList);
+            model.addAttribute("uniqueTimeList", uniqueTimeList);
+        }
+        // 데이터 날짜 값 받아서 시간 배열 만드는 로직
         return "lesson";
     }
 
     @GetMapping("/multi_box_ajax")
-    public String selectedData2(Model model, @RequestParam("data") LocalDate data) {
-        List<Lesson> lessonTimeList = lessonService.getTimeList(data);
+    @ResponseBody
+    public List<Lesson> selectedData2(@RequestParam LocalDate data) {
+        lessonTimeList = lessonService.getTimeList(data);
         System.out.println(data);
 
         Iterator<Lesson> iterator = lessonTimeList.iterator();
@@ -40,8 +67,19 @@ public class LessonController {
             Lesson element = iterator.next();
             System.out.println(element);
         }
-
-        model.addAttribute("lessonTimeList", lessonTimeList);
-        return "lesson";
+        return lessonTimeList;
     }
+
+//    @GetMapping("/multi_box_ajax")
+//    @ResponseBody
+//    public List<Lesson> selectedData3(Model model, LocalDate data){
+//        lessonTimeList = lessonService.getTimeList(data);
+//        Iterator<Lesson> iterator = lessonTimeList.iterator();
+//
+//        while (iterator.hasNext()) {
+//            Lesson element = iterator.next();
+//            System.out.println(element);
+//        }
+//        return lessonTimeList;
+//    }
 }
