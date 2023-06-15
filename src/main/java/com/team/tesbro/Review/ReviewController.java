2e380faf238ea31ebe004320784e3af53e7ce691
Jserin <jsr3643@naver.com>
2023-06-15 19:16:54 +0900

    머승열머지전

--- src/main/java/com/team/tesbro/Review/ReviewController.java
+++ src/main/java/com/team/tesbro/Review/ReviewController.java
@@ -1,7 +1,26 @@
 package com.team.tesbro.Review;
 
+import com.team.tesbro.Academy.Academy;
+import com.team.tesbro.Academy.AcademyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping("/review")
+@RequiredArgsConstructor
 @Controller
 public class ReviewController {
+    private final AcademyService academyService;
+    private final ReviewService reviewService;
+
+    @PostMapping("/create/{id}")
+    public String createReview(Model model, @PathVariable("id") Integer id, @RequestParam String content, @RequestParam int star_rating){
+        Academy academy = this.academyService.getAcademy(id);
+        this.reviewService.create(academy, content, star_rating);
+        return String.format("redirect:/academy/detail/%s", id);
+    }
 }
--- src/main/java/com/team/tesbro/Review/ReviewService.java
+++ src/main/java/com/team/tesbro/Review/ReviewService.java
@@ -1,7 +1,24 @@
 package com.team.tesbro.Review;
 
+import com.team.tesbro.Academy.Academy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
 @Service
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public void create(Academy academy, String content, int star_rating){
+        Review review = new Review();
+        review.setContent(content);
+        review.setCreateDate(LocalDateTime.now());
+        review.setModifyDate(LocalDateTime.now());
+        review.setStar_rating(star_rating);
+        review.setUserId("testid");
+        review.setAcademy(academy);
+        this.reviewRepository.save(review);
+    }
 }
