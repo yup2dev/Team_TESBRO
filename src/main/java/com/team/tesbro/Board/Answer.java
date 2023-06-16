151e317dbc56ed91260c9767e0d173fd93fc333f
Jserin <jsr3643@naver.com>
2023-06-16 11:09:44 +0900

    승열머지완

---
+++ src/main/java/com/team/tesbro/Board/Answer.java
@@ -0,0 +1,27 @@
+package com.team.tesbro.Board;
+
+import com.team.tesbro.User.SiteUser;
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne
+    private SiteUser author;
+
+}
---
+++ src/main/java/com/team/tesbro/Board/AnswerForm.java
@@ -0,0 +1,11 @@
+package com.team.tesbro.Board;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnswerForm {
+    private Integer boardId;
+    private String content;
+}
