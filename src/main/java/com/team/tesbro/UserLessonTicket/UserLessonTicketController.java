98b93f9ddd4f11e361f1c2cfc667aa5da77ab155
sangyeopKim <creativethony@naver.com>
2023-06-27 22:03:48 +0900

    좀 더 쉬워진거같기도한 꺼림찍한 느김?

---
+++ src/main/java/com/team/tesbro/UserLessonTicket/UserLessonTicketController.java
@@ -0,0 +1,7 @@
+package com.team.tesbro.UserLessonTicket;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserLessonTicketController {
+}
--- src/main/java/com/team/tesbro/UserLessonTicket/UserLessonTicketService.java
+++ src/main/java/com/team/tesbro/UserLessonTicket/UserLessonTicketService.java
@@ -1,27 +1,26 @@
 package com.team.tesbro.UserLessonTicket;
 
 import com.team.tesbro.DataNotFoundException;
+import com.team.tesbro.LessonTicket.LessonTicket;
 import com.team.tesbro.LessonTicket.LessonTicketDto;
 import com.team.tesbro.User.SiteUser;
 import com.team.tesbro.User.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
+@RequiredArgsConstructor
 public class UserLessonTicketService {
-    UserLessonTicketRepository userLessonTicketRepository;
-    UserRepository userRepository;
-    public void addTicketUser(Integer ticketId, Integer siteUserId, LessonTicketDto lessonTicketDto) {
-        Optional<UserLessonTicket> ulto = userLessonTicketRepository.findById(ticketId);
-        if (ulto.isPresent()) {
-            UserLessonTicket UlessonTicket = ulto.get();
-            lessonTicketDto.getUserIds().add(siteUserId);
-
-            for (Integer users : lessonTicketDto.getUserIds()) {
-                Optional<SiteUser> userOptional = userRepository.findById(users);
-                SiteUser user = userOptional.orElseThrow(() -> new DataNotFoundException("존재하지 않는 사용자 ID입니다"));
-                user = UlessonTicket.getSiteUser();
-            }
-            userLessonTicketRepository.save(UlessonTicket);
-        }
+    private final UserLessonTicketRepository userLessonTicketRepository;
+    private final UserRepository userRepository;
+    public UserLessonTicket addUserLessonTicket(SiteUser siteUser, LessonTicket lessonTicket) {
+        UserLessonTicket userLessonTicket = new UserLessonTicket();
+        userLessonTicket.setSiteUser(siteUser);
+        userLessonTicket.setLessonTicket(lessonTicket);
+        userLessonTicket.setUsedTicketTime(0);
+        //여기까지 하고 쉬러간다
+        return userLessonTicketRepository.save(userLessonTicket);
     }
 }
